66b799beac4a49705f2ec5c8413606210a720e5a
Alexander Erfurt <icloud3009@gmail.com>
2024-03-11 22:16:24 +0100

    Update

--- src/components/CreateTransaction.vue
+++ src/components/CreateTransaction.vue
@@ -5,21 +5,23 @@
         <h3>Add Transaction</h3>
       </div>
       <div class="card-body">
-        <div class="form-group">
-          <label for="transactionAmount">Amount</label>
-          <input type="number" class="form-control" v-model="transactionData.amount" id="transactionAmount">
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="radio" id="depositRadio" value="DEPOSIT" v-model="transactionData.type">
-          <label class="form-check-label" for="depositRadio">Deposit</label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="radio" id="withdrawalRadio" value="WITHDRAWAL" v-model="transactionData.type">
-          <label class="form-check-label" for="withdrawalRadio">Withdrawal</label>
+        <div class="row">
+          <div class="form-group col-md-8">
+            <label for="transactionAmount">Amount</label>
+            <input type="number" class="form-control" v-model="transactionData.amount" id="transactionAmount">
+          </div>
+          <div class="form-group col-md-4">
+            <label for="transactionType">Transaction Type</label>
+            <select id="transactionType" class="form-control" v-model="transactionData.type">
+              <option value="DEPOSIT">Deposit</option>
+              <option value="WITHDRAWAL">Withdrawal</option>
+            </select>
+          </div>
         </div>
         <button class="btn btn-primary mt-3" @click="createTransaction">Add Transaction</button>
         <router-link to="/user" class="btn btn-secondary mt-3">Back</router-link>
       </div>
+
     </div>
 
     <div v-if="showSuccessAlert" class="alert alert-success mt-3" role="alert">
@@ -49,6 +51,9 @@ export default {
           response => {
             console.log('Transaction created successfully:', response.data);
             this.showSuccessAlert = true;
+            setTimeout(() => {
+              this.$router.push('/user');
+            }, 500);
           },
           error => {
             console.error('Error creating transaction:', error);
@@ -56,5 +61,6 @@ export default {
       );
     }
   }
+
 };
 </script>
